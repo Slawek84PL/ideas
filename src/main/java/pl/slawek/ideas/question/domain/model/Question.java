188a5dff58f5af8d1f6e31228f2590366c01fa8e
Slawek <slapyt84@gmail.com>
2023-09-18 12:25:04 +0200

    Add UUID

---
+++ src/main/java/pl/slawek/ideas/question/domain/model/Question.java
@@ -0,0 +1,43 @@
+package pl.slawek.ideas.question.domain.model;
+
+
+import java.util.UUID;
+
+class Question {
+
+    private UUID id;
+
+    private String name;
+
+    Question() {
+    }
+
+    Question(final String name) {
+        this.name = name;
+        this.id = UUID.randomUUID();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    void setName(final String name) {
+        this.name = name;
+    }
+
+    UUID getId() {
+        return id;
+    }
+
+    void setId(final UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
