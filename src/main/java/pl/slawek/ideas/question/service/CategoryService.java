82adcb4842f4c8674dfd60d8645ef6871d9ba70b
Slawek <slapyt84@gmail.com>
2023-10-11 18:28:42 +0200

    Added CategoryService

---
+++ src/main/java/pl/slawek/ideas/question/service/CategoryService.java
@@ -0,0 +1,48 @@
+package pl.slawek.ideas.question.service;
+
+import org.springframework.stereotype.Service;
+import pl.slawek.ideas.question.domain.model.Category;
+import pl.slawek.ideas.question.exceptions.NotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class CategoryService {
+
+    private final List<Category> categoryList = new ArrayList<>();
+
+    public List<Category> getCategories() {
+        return categoryList;
+    }
+
+    public Category createCategory(final Category category) {
+        Category newCategory = new Category(category.getName());
+        categoryList.add(newCategory);
+        return newCategory;
+    }
+
+    public Category updateCategory(final UUID id, final Category category) {
+        Category oldCategory = categoryService(id);
+        oldCategory.setName(category.getName());
+        return oldCategory;
+    }
+
+    public void deleteCategory(final UUID id) {
+        categoryList.remove(categoryService(id));
+    }
+
+    public Category getCategory(final UUID id) {
+        return categoryService(id);
+    }
+
+    private Category categoryService(final UUID id) {
+        for (Category category : categoryList) {
+            if (category.getId().equals(id)) {
+                return category;
+            }
+        }
+        throw new NotFoundException("category", id);
+    }
+}
