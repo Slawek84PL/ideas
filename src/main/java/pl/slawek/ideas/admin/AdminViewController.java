2d7b8baa260e632e310495d77cbfe231d894679c
Slawek <slapyt84@gmail.com>
2023-10-15 12:27:55 +0200

    Added template for admin view. Added AdminViewController Update SingleView.

---
+++ src/main/java/pl/slawek/ideas/admin/AdminViewController.java
@@ -0,0 +1,15 @@
+package pl.slawek.ideas.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+class AdminViewController {
+
+    @GetMapping
+    public String indexView() {
+        return "admin/index";
+    }
+}
