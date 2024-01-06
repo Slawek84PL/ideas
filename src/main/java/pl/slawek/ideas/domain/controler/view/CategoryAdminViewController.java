52020f74d1e2873d204b7f04eab2b4aa0d1988c5
Slawek <slapyt84@gmail.com>
2024-01-06 15:55:26 +0100

    Added sort

--- src/main/java/pl/slawek/ideas/domain/controler/view/CategoryAdminViewController.java
+++ src/main/java/pl/slawek/ideas/domain/controler/view/CategoryAdminViewController.java
@@ -2,7 +2,9 @@ package pl.slawek.ideas.domain.controler.view;
 
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -28,10 +30,22 @@ class CategoryAdminViewController {
     }
 
     @GetMapping
-    public String indexView(@RequestParam(name = "s", required = false) String search, Pageable pageable, Model model) {
+    public String indexView(
+            @RequestParam(name = "s", required = false) String search,
+            @RequestParam(name = "field", required = false, defaultValue = "id") String field,
+            @RequestParam(name = "direction", required = false, defaultValue = "asc") String direction,
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(name = "size", required = false, defaultValue = "10") int size,
+            Model model) {
+
+        Pageable pageable = PageRequest.of(page, size, Sort.Direction.fromString(direction), field);
+
+        String reverseSort = "asc".equals(direction) ? "desc" : "asc";
+
         Page<Category> categoriesPage = categoryService.getCategories(search, pageable);
         model.addAttribute("categoriesPage", categoriesPage );
         model.addAttribute("search", search);
+        model.addAttribute("reverseSort", reverseSort);
         paging(model, categoriesPage);
         return "admin/category/index";
     }
