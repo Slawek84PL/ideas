5a39c0f72aa048266ff64f38fd519a1256ee43fc
Slawek84PL <slapyt84@gmail.com>
2024-05-28 20:22:32 +0200

    Add Lombok annotation

--- src/main/java/pl/slawek/ideas/domain/model/Question.java
+++ src/main/java/pl/slawek/ideas/domain/model/Question.java
@@ -1,13 +1,21 @@
 package pl.slawek.ideas.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-import jakarta.persistence.*;
-
-import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.UUID;
 
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "questions")
 public class Question {
@@ -41,43 +49,5 @@ public class Question {
         answers.add(answer);
 
         return this;
-    }
-
-    public Set<Answer> getAnswers() {
-        return Collections.unmodifiableSet(answers);
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(final UUID id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Question{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
     }
 }
